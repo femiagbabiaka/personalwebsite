f30fc81a3fc908f4f324ce687bca9b8d75309ef2
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2019-09-30 00:10:55 +0000

    Start categories branch

--- src/personalwebsite/controllers/posts.clj
+++ src/personalwebsite/controllers/posts.clj
@@ -6,19 +6,32 @@
               [clojure.java.io :as io]
               [clojure.string :as str]))
 
-(defn is-valid-slug? [slug]
+(defn is-valid-folder? [slug]
   (cond
     (str/blank? slug) false
     (.exists (io/as-file (str "/srv/personalwebsite/docs/" slug))) true
     :else false))
 
-(defn singlePost [slug]
-  (if (is-valid-slug? slug)
-    (layout/single slug)
+(defn is-valid-post? [folder slug]
+  (cond
+    (str/blank? slug) false
+    (str/blank? folder) false
+    (.exists (io/as-file (str "/srv/personalwebsite/docs/" folder "/" slug))) true
+    :else false))
+
+(defn singleFolder [slug]
+  (if (is-valid-folder? slug)
+    (layout/folderPage slug)
+    (layout/postNotFound)))
+
+(defn singlePost [folder slug]
+  (if (is-valid-post? folder slug)
+    (layout/single folder slug)
     (layout/postNotFound)))
 
 (compojure/defroutes routes
     (compojure/GET "/" [] (layout/index))
     (compojure/GET "/blog" [] (layout/blogPage))
-    (compojure/GET "/blog/:slug" [slug] (singlePost slug))
+    (compojure/GET "/blog/:folder" [folder] (singleFolder folder))
+    (compojure/GET "/blog/:folder/:post" [folder post] (singlePost folder post))
     (compojure-route/not-found (layout/postNotFound)))
